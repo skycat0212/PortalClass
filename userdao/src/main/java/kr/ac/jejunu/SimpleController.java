b63c4a33e065ec02f1bd75e2c427e4e79c57f9a4
skycat0212 <mr_magnet@naver.com>
2021-05-28 18:08:26 +0900

    view resolver

--- userdao/src/main/java/kr/ac/jejunu/SimpleController.java
+++ userdao/src/main/java/kr/ac/jejunu/SimpleController.java
@@ -1,21 +1,32 @@
 package kr.ac.jejunu;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@org.springframework.stereotype.Controller
+@RequestMapping
+
 @RequiredArgsConstructor
-public class SimpleController implements Controller {
+//public class SimpleController implements Controller {
+public class SimpleController {
+
     private final UserDao userDao;
 
-    @Override
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        User user = userDao.findById(Integer.valueOf(request.getParameter("id")));
-        ModelAndView modelAndView = new ModelAndView("user");
-        modelAndView.addObject("user", user);
-        return modelAndView;
+    @RequestMapping("/user")
+    public User getUser(@RequestParam("id") Integer id) {
+        return userDao.findById(id);
     }
+//    @Override
+//    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//        User user = userDao.findById(Integer.valueOf(request.getParameter("id")));
+//        ModelAndView modelAndView = new ModelAndView("user");
+//        modelAndView.addObject("user", user);
+//        return modelAndView;
+//    }
 }
